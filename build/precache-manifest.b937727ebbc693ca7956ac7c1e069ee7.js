8e6e56f8667ba2a729697c02627fde84d5fdf52e
Taras <taras.prezlyata.sign.in@gmail.com>
2019-03-17 00:47:07 +0200

    first commit

---
+++ build/precache-manifest.b937727ebbc693ca7956ac7c1e069ee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "414993ec9fd3a2ba4bcd",
+    "url": "/static/js/main.804a03b5.chunk.js"
+  },
+  {
+    "revision": "12f05e3f531efa92f25e",
+    "url": "/static/js/2.a94ab642.chunk.js"
+  },
+  {
+    "revision": "414993ec9fd3a2ba4bcd",
+    "url": "/static/css/main.e3bf3f19.chunk.css"
+  },
+  {
+    "revision": "0227cc331e368be1631bd6979899b48f",
+    "url": "/index.html"
+  }
+];
